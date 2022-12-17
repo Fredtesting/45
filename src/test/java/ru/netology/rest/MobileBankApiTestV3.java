0e17566c474f17a0392574960725987165a8806f
Voronchikhin Fedor <fredsergeevich@outlook.com>
2022-12-17 19:42:58 +0300

    Test currency

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -27,4 +27,61 @@ class MobileBankApiTestV3 {
           .body("[0].balance", greaterThanOrEqualTo(0))
       ;
     }
+    @Test
+    void shouldReturnDemoAccounts2() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("USD"))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+        ;
+    }
+    @Test
+    void shouldReturnDemoAccounts3() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[2].currency", equalTo("RUR"))
+                .body("[2].balance", greaterThanOrEqualTo(0))
+        ;
+    }
+    @Test
+    void shouldReturnDemoAccounts4() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                .body("", hasSize(3))
+                .body("[1].currency", equalTo("RUB"))
+                .body("[1].balance", greaterThanOrEqualTo(0))
+        ;
+    }
 }
